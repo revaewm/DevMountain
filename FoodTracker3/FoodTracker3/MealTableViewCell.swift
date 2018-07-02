47f52c14dc3fe90106b6b52efb66aa3441972baa
Matt Weaver <35481689+revaewm@users.noreply.github.com>
2018-07-01 23:34:05 -0600

    Foodtracker version 3 upload
    initial upload of foodtraker3

---
+++ FoodTracker3/FoodTracker3/MealTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker3
+//
+//  Created by Matthew Weaver on 7/1/18.
+//  Copyright © 2018 Matthew Weaver. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodTracker3/FoodTracker3Tests/FoodTracker3Tests.swift
@@ -0,0 +1,44 @@
+//
+//  FoodTracker3Tests.swift
+//  FoodTracker3Tests
+//
+//  Created by Matthew Weaver on 6/30/18.
+//  Copyright © 2018 Matthew Weaver. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker3
+
+class FoodTracker3Tests: XCTestCase {
+    
+    //MARK: Meal Class Test
+    
+    //Confirm that the Meal initializer returns a Meal object when pass valid parameters
+    func testMealInitializationSucceeds() {
+        
+        //Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        //Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    //Confirm that the Meal initializer returns nil when passed a negative rating or an empty name
+    func testMealInitializatonFails(){
+        
+        //Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        //Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        //Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
+    
+}//End of XCText case class
